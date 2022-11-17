15cc3691ba9b95a85b0ab73340d80c67af7155d2
sanmacorz <sanmacorz@protonmail.com>
2022-11-16 21:44:11 -0500

    Agregando cambios finales en los archivos del proyecto

--- README.md
+++ README.md
@@ -1,23 +1,29 @@
-# Backtracking sudoku
+# Resolución de Sudoku
 
-Este es un programa en Python
+![GitHub](https://img.shields.io/github/license/sanmacorz/resolucion-sudoku)
+![GitHub](https://img.shields.io/github/commit-activity/m/sanmacorz/resolucion-sudoku)
 
-3 0 6 5 0 8 4 0 0
-5 2 0 0 0 0 0 0 0
-0 8 7 0 0 0 0 3 1
-0 0 3 0 1 0 0 8 0
-9 0 0 8 6 3 0 0 5
-0 5 0 0 9 0 6 0 0
-1 3 0 0 0 0 2 5 0
-0 0 0 0 0 0 0 7 4
-0 0 5 2 0 6 3 0 0
+Este proyecto es una aplicación capaz de resolver cualquier [sudoku](https://es.wikipedia.org/wiki/Sudoku) que se le configure, para esto utiliza el algoritmo de [vuelta atrás](https://es.wikipedia.org/wiki/Vuelta_atr%C3%A1s), el cuál es una [búsqueda en profundidad](https://es.wikipedia.org/wiki/B%C3%BAsqueda_en_profundidad), la cuál támbien podría ser considerada como un algoritmo de [fuerza bruta](https://es.wikipedia.org/wiki/B%C3%BAsqueda_de_fuerza_bruta).  
+Todo el programa está desarrollado en el lenguaje de programación [Python](https://es.wikipedia.org/wiki/Python) e implementa su interfaz gráfica usando la librería nativa [Tkinter](https://es.wikipedia.org/wiki/Tkinter).
 
-3 1 6 5 7 8 4 9 2
-5 2 9 1 3 4 7 6 8
-4 8 7 6 2 9 5 3 1
-2 6 3 4 1 5 9 8 7
-9 7 4 8 6 3 1 2 5
-8 5 1 7 9 2 6 4 3
-1 3 8 9 4 7 2 5 6
-6 9 2 3 5 1 8 7 4
-7 4 5 2 8 6 3 1 9
+### Configuración del proyecto
+
+```bash
+git clone https://github.com/sanmacorz/resolucion-sudoku.git
+cd resolucion-sudoku/
+```
+
+### Uso del proyecto
+
+```bash
+cd src/
+python3 main.py
+```
+
+## Capturas de pantalla
+
+La interfaz gráfica del proyecto mostrando el resultado de un sudoku:
+![Screenshot1](https://user-images.githubusercontent.com/27830167/202340360-0bca045d-a3a0-41e5-b07a-6d8606df088f.png)
+La aplicación [Ksudoku](https://github.com/KDE/ksudoku) mostrando el mismo resultado, nótese que el algoritmo utilizado aquí es el de [Dancing Links](https://en.wikipedia.org/wiki/Dancing_Links):
+
+![Screenshot2](https://user-images.githubusercontent.com/27830167/202340371-2e2921a4-b613-48bc-b8ef-f31c1d89490d.png)
--- src/main.py
+++ src/main.py
@@ -1,83 +1,182 @@
 #!/usr/bin/env python3
-import tkinter as tk
-from funciones import resolver_sudoku
 
+# Se importa el módulo tkinter para la interfaz
+import tkinter as tk
+
+# Crea la ventana principal y define sus propiedades
 ventana_principal = tk.Tk()
-ventana_principal.geometry("1280x720")
+ventana_principal.geometry("600x700")
 ventana_principal.resizable(False, False)
-ventana_principal.configure(background="#4DBFD9")
+ventana_principal.configure(background="#9B9B9B")
 ventana_principal.title("Resolución de Sudoku")
 
+# Crea el frame principal y define sus propiedades
 frame_principal = tk.Frame(
     ventana_principal,
-    width="1120",
-    height="540",
-    highlightthickness=5,
-    highlightbackground="#4D4D4D",
+    width="600",
+    height="700",
 )
 frame_principal.configure(background="#FAFAFA")
-frame_principal.place(relx=0.5, rely=0.55, anchor=tk.CENTER)
+frame_principal.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
+# Crea el canvas principal y define sus propiedades
 canvas_principal = tk.Canvas(
     frame_principal,
-    width="1024",
-    height="576",
-    highlightthickness=0,
-    highlightbackground="#4D4D4D",
+    width="600",
+    height="700",
+    highlightthickness=3,
+    highlightbackground="#000000",
 )
 canvas_principal.configure(background="#FAFAFA")
-canvas_principal.place(relx=0.5, rely=0.55, anchor=tk.CENTER)
+canvas_principal.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-# titulo_proyecto = tk.Label(ventana_principal, text="Resolución de Sudoku")
-# titulo_proyecto.config(background="#4DBFD9", font=("Arial", 16, "bold"))
-# titulo_proyecto.place(x=550, y=50)
+# Se define una matriz para los valores a manejar por el algoritmo
+valores = []
 
-# logo = tk.PhotoImage(file="logo.png")
-# logo_label = tk.Label(ventana_principal, background="#4DBFD9", image=logo)
-# logo_label.place(x=1000, y=15)
+# Se recorre todas las columnas del array para añadir las filas
+# Esto se hace así para poder acceder a los métodos set y get
+for i in range(1, 10):
+    valores += [[0, 0, 0, 0, 0, 0, 0, 0, 0]]
+for i in range(0, 9):
+    for j in range(0, 9):
+        valores[i][j] = tk.StringVar(ventana_principal)
 
-# cuadricula = [
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-# ]
+# Se define una función, la cuál define todos los valores de las casillas vacías en 0 e inicia el algoritmo de vuelta atrás para buscar la solución
+def resolver_sudoku():
+    llenar_cuadricula()
+    empezar_solucion()
 
-# Agregar for que cree todas las casillas 9x9 = 81 casillas / 3 bloques por lado
-for coordenada_x in range(0, 360, 40):
-    for coordenada_y in range(0, 360, 40):
-        tk.Entry(ventana_principal).place(
-            x=coordenada_x, y=coordenada_y, width=40, height=40
+
+# Lee los valores de toda la matriz y si alguno es un número no válido, lo define como un cero
+def llenar_cuadricula():
+    for i in range(9):
+        for j in range(9):
+            if valores[i][j].get() not in [
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+            ]:
+                valores[i][j].set(0)
+
+
+# Se define la función que busca a la celda más cercana que se encuentre vacía
+def hallar_casilla_vacia(i, j):
+    for x in range(i, 9):
+        for y in range(j, 9):
+            if valores[x][y].get() == "0":
+                return x, y
+    for x in range(0, 9):
+        for y in range(0, 9):
+            if valores[x][y].get() == "0":
+                return x, y
+    return -1, -1
+
+
+# Se define la función que verifica qué números se encuentran disponibles para utilizarse dada una casilla en especifico
+def verificar_numero(i, j, e):
+    for x in range(9):
+        # Verifica si el número se encuentra en la misma fila
+        if valores[i][x].get() == str(e):
+            return False
+    for x in range(9):
+        # Verifica si el número se encuentra en la misma columna
+        if valores[x][j].get() == str(e):
+            return False
+    cuadricula_horizontal, cuadricula_vertical = 3 * int((i / 3)), 3 * int((j / 3))
+    for x in range(cuadricula_horizontal, cuadricula_horizontal + 3):
+        for y in range(cuadricula_vertical, cuadricula_vertical + 3):
+            # Verifica si el número está en la misma cuadricula
+            if valores[x][y].get() == str(e):
+                return False
+    # Si devuelve verdadero es porque el número se puede utilizar en la casilla
+    return True
+
+
+# Se define la función que implementa el algoritmo de vuelta atrás
+# Se empieza definiendo una posición inicial en el origen de la matriz es decir de cero y cero
+def empezar_solucion(i=0, j=0):
+    i, j = hallar_casilla_vacia(i, j)
+    # Si i es igual a menos uno el sudoku ya está resuelto porque no tiene ninguna casilla vacía
+    if i == -1:
+        return True
+    for e in range(1, 10):
+        if verificar_numero(i, j, e):
+            valores[i][j].set(e)
+            # Se vuelve a llamar a la función recursivamente para que se ejecute hasta que solucione el sudoku
+            if empezar_solucion(i, j):
+                # Si se devuelve verdadero es porque el sudoku acaba de ser resuelto y el "ciclo" puede terminar
+                return True
+            # Se devuelve una posición por la raíz del grafo
+            valores[i][j].set(0)
+    # Si se devuelve falso es porque la configuración dada para el sudoku no tiene solución
+    return False
+
+
+# Se define la función que recorre toda la matriz y reinicia los valores de cada casilla
+def borrar():
+    for i in range(9):
+        for j in range(9):
+            valores[i][j].set("")
+
+
+# Crea una label y define sus propiedades
+titulo_proyecto = tk.Label(ventana_principal, text="Resolución de Sudoku")
+titulo_proyecto.config(background="#9B9B9B", font=("Arial", 16, "bold"))
+titulo_proyecto.place(x=300, y=50, anchor=tk.CENTER)
+
+# Se define una matriz del mismo tamaño para las casillas de texto mostradas en la interfaz
+cuadricula = []
+
+# Se recorre todas las columnas del array para añadir las filas
+# Esto se hace así para poder acceder a los métodos set y get
+for i in range(1, 10):
+    cuadricula += [[0, 0, 0, 0, 0, 0, 0, 0, 0]]
+for i in range(0, 9):
+    for j in range(0, 9):
+        cuadricula[i][j] = tk.Entry(
+            frame_principal,
+            width=2,
+            font=("Arial", 18),
+            bg="white",
+            cursor="arrow",
+            borderwidth=0,
+            highlightcolor="yellow",
+            highlightthickness=1,
+            highlightbackground="black",
+            textvar=valores[i][j],  # type: ignore
         )
+        # Se especifica un padding interno para aumentar el tamaño de las casillas
+        cuadricula[i][j].grid(row=i, column=j, ipadx=12, ipady=12)
 
-# Agregar botón
+# Crea un botón y define sus propiedades
+boton_resolver = tk.Button(
+    ventana_principal,
+    text="Resolver",
+    font=("Arial", 16, "bold"),
+    command=resolver_sudoku,
+)
+boton_resolver.place(x=180, y=650, anchor=tk.CENTER)
+
+# Crea un botón y define sus propiedades
+boton_borrar = tk.Button(
+    ventana_principal, text="Borrar", font=("Arial", 16, "bold"), command=borrar
+)
+boton_borrar.place(x=300, y=650, anchor=tk.CENTER)
+
+# Crea un botón y define sus propiedades
+boton_salir = tk.Button(
+    ventana_principal,
+    text="Salir",
+    font=("Arial", 16, "bold"),
+    command=ventana_principal.quit,
+)
+boton_salir.place(x=400, y=650, anchor=tk.CENTER)
+
+# Utiliza el método mainloop nativo del módulo tkinter para detener la alocación de recursos
 ventana_principal.mainloop()
-
-# def set_text(text):
-#     e.delete(0,END)
-#     e.insert(0,text)
-#     return
-#
-# win = Tk()
-#
-# e = Entry(win,width=10)
-# e.pack()
-#
-# b1 = Button(win,text="animal",command=lambda:set_text("animal"))
-# b1.pack()
-# boton que muestre la respuesta
-# messagebox.askyesno("Resultado", "La solucionn de la dialectica")
-
-# if resolver_sudoku(cuadricula):
-#     for i in range(9):
-#         for j in range(9):
-#             print(cuadricula[i][j], end=" "),
-#         print()
-# else:
-#     print("No existe solución!")
-#
